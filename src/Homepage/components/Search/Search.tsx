325df467e369dbbb2a80dbb6f769fcd76d196715
Arkadiusz Kalbarczyk <arkadiusz.kalbarczyk@pekao-fs.com.pl>
2020-02-16 20:54:42 +0100

    iceland logo and style

--- src/Homepage/components/Search/Search.tsx
+++ src/Homepage/components/Search/Search.tsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 
 import "./Search.scss";
-
+import IslandLogo from "../../../assets/icons/iceland-map.png";
 interface Props {}
 class Search extends Component<Props> {
   render() {
     return (
       <section className="Search">
+        <figure className="Search-country">
+          <img src={IslandLogo} alt="map" />
+        </figure>
         <section className="Search-container">
           <label>
             <h3 className="Search-label">Search for</h3>
